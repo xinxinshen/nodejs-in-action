98c8bf4fabb220e487f6ddd36ba08e162fa2375b
xinxinshen <xinxinshen@github.com>
2018-02-21 02:38:17 +0800

    init

---
+++ current.js
@@ -0,0 +1,5 @@
+setInterval(()=>{
+    var d = new Date();
+
+    console.log(`现在是${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日${d.getHours()}时${d.getMinutes()}分${d.getSeconds()}秒`)
+    }, 1000);
---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log('hello world!')
+console.log(__filename)
+console.log(global)
